7766ba3a37da46cb9f2b97c3081791543dcaa904
Daniel Choi <dhchoi@gmail.com>
2012-10-05 18:28:22 -0400

    More ToJSON instances

--- src/GeoChat/Types.hs
+++ src/GeoChat/Types.hs
@@ -1,29 +1,40 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module GeoChat.Types where
 
 import Data.Text (Text)
+import Data.Vector (fromList)
 import qualified Network.WebSockets as WS
 import Control.Applicative
 import Control.Monad (MonadPlus, mzero)
 import qualified Data.HashMap.Strict as M
+import GHC.Generics (Generic)
 import Data.Aeson 
 
 instance Show (WS.Sink a) where
     show _ = "[WS.Sink value]"
+
+instance ToJSON (WS.Sink a) where
+    toJSON _ = toJSON ()   
 
 type LatLng = (Double, Double)
 
 data Room = Room { roomId :: Int 
                  , latLng :: LatLng 
                  , numParticipants :: Int
-                 } deriving (Show)
+                 } deriving (Generic, Show)
+
+instance ToJSON Room
 
 data Client = Client { clientId :: Int
                      , nickname :: Text
                      , clientSink :: Maybe (WS.Sink WS.Hybi00)
                      , clientRoom :: Maybe Room 
-                     } deriving (Show)
+                     } deriving (Generic, Show)
+
+instance ToJSON Client
+
 type RoomId = Int
 type ClientId = Int
 
@@ -35,25 +46,55 @@ data MessageFromClient = ListActiveRooms   -- TODO scope by latLng center
                        | ChangeNickname Text
                        | PostMessage ClientId Text deriving (Show)
 
+instance FromJSON MessageFromClient where
+  parseJSON (Object v) 
+    | Just "Enter" <- M.lookup "type" v = Enter <$> v .: "clientId" <*> v .: "roomId"
+    | Just "Exit" <- M.lookup "type" v = Exit <$> v .: "clientId" <*> v .: "roomId"
+    | Just "NewClient" <- M.lookup "type" v = NewClient <$> v .: "nickname" 
+    | Just "CreateRoom" <- M.lookup "type" v = CreateRoom <$> ((,) <$> v .: "lat" <*>  v .: "lng")
+    | otherwise  = mzero
+  parseJSON _ = mzero
+
 data MessageFromServer = ListOfActiveRooms [Room]
                        | NewClientCreated Client
                        | RoomActivity Room
-                       | BroadcastToRoom Room Text -- need to add author or make ChatMessage type
+                       | Broadcast Room Text -- need to add author or make ChatMessage type
                        | NewRoom Room
                        | UpdatedRoom Room
                        | DeadRoom Room deriving (Show)
 
--- test in ghci:
--- (decode $ pack "{\"type\": \"Enter\", \"clientId\": 2, \"roomId\": 3}")::Maybe MessageFromClient
--- Just (Enter 2 3)
---
--- (decode $ pack "{\"type\": \"CreateRoom\", \"lat\": 43.3, \"lng\": -70.1}")::Maybe MessageFromClient
+instance ToJSON MessageFromServer where
+  toJSON (ListOfActiveRooms rooms) = object ["type" .= ("ListOfActiveRooms" :: Text), "rooms" .= (fromList rooms)]
+  toJSON (NewClientCreated c) = object ["type" .= ("NewClientCreated" :: Text), "client" .= c]
+  toJSON (RoomActivity room) = object ["type" .= ("RoomActivity" :: Text), "room" .= room]
+  toJSON (NewRoom room) = object ["type" .= ("NewRoom" :: Text), "room" .= room]
+  toJSON (UpdatedRoom room) = object ["type" .= ("UpdatedRoom" :: Text), "room" .= room]
+  toJSON (DeadRoom room) = object ["type" .= ("DeadRoom" :: Text), "room" .= room]
+  toJSON (Broadcast room text) = object ["type" .= ("BroadCast" :: Text), "room" .= room, "text" .= text]
 
-instance FromJSON MessageFromClient where
-  parseJSON (Object v) | Just "Enter" <- M.lookup "type" v = Enter <$> v .: "clientId" <*> v .: "roomId"
-                       | Just "Exit" <- M.lookup "type" v = Exit <$> v .: "clientId" <*> v .: "roomId"
-                       | Just "NewClient" <- M.lookup "type" v = NewClient <$> v .: "nickname" 
-                       | Just "CreateRoom" <- M.lookup "type" v = CreateRoom <$> ((,) <$> v .: "lat" <*>  v .: "lng")
-                       | otherwise  = mzero
-  parseJSON _ = mzero
+
+
+{- 
+
+ (decode $ pack "{\"type\": \"Enter\", \"clientId\": 2, \"roomId\": 3}")::Maybe MessageFromClient
+ Just (Enter 2 3)
+
+ (decode $ pack "{\"type\": \"CreateRoom\", \"lat\": 43.3, \"lng\": -70.1}")::Maybe MessageFromClient
+ Just (CreateRoom (43.3,-70.1))
+-}
+
+{-
+  encode (NewClientCreated Client { clientId = 12, nickname = Text.pack "dan" , clientSink = Nothing , clientRoom = Nothing})
+  Chunk "{\"clientId\":12,\"type\":\"NewClientCreated\"}" Empty
+
+  Experiment:
+  encode (ListOfActiveRooms [])
+
+encode (ListOfActiveRooms [Room { roomId = 3 , latLng = (40.2, -71.2), numParticipants = 0}])
+Chunk "{\"rooms\":[{\"latLng\":[40.2,-71.2],\"numParticipants\":0,\"roomId\":3}],\"type\":\"ListOfActiveRooms\"}" Empty
+
+encode (RoomActivity Room { roomId = 3 , latLng = (40.2, -71.2), numParticipants = 0})
+
+
+-}
 
