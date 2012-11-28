4c4b41cdacd34929952b9d331ac675fc59524553
Daniel Choi <dhchoi@gmail.com>
2012-11-28 00:36:38 -0500

    refactoring core user funcs

--- src/Core.hs
+++ src/Core.hs
@@ -12,23 +12,21 @@ countAllUsers = do
     dbh <- conn
     [[r :: SqlValue]] <- quickQuery' dbh "select count(*) from users" []
     return $ fromSql r
-  
+
 createUser :: String -> IO User
 createUser name = do
     dbh <- conn
-    [[r :: SqlValue]] <- quickQuery' dbh "insert into users (name) values (?) returning user_id" [toSql name]
+    [r] <- quickQuery' dbh 
+         ("insert into users (name) values (?) returning " ++ userFields) [toSql name]
     commit dbh
-    return $ User (fromSql r) name Nothing Nothing 
+    return $ convUserRow r
 
 allUsers :: IO [User]
 allUsers = do
     dbh <- conn
-    r <- quickQuery' dbh "select user_id, name, room_id from users" []
+    r <- quickQuery' dbh ("select " ++ userFields ++ " from users") []
     return $ map convUserRow r
-    where convUserRow :: [SqlValue] -> User
-          convUserRow [a, b, c] = 
-            User (fromSql a) (fromSql b) Nothing (fromSql c)
- 
+
 setUserCoordinates :: Int -> Coordinates -> IO ()
 setUserCoordinates userId (lat, lng) = do
     dbh <- conn
@@ -39,22 +37,29 @@ setUserCoordinates userId (lat, lng) = do
     commit dbh
     return ()
  
+
 getUser :: Int -> IO User
 getUser uid = do
     dbh <- conn
     -- TODO Handle no results
-    [[a, b, lat, lng, c]] <- quickQuery' dbh 
-                     "select user_id, name, \
-                     \st_y(st_transform(coordinates, 4326)), \  
-                     \st_x(st_transform(coordinates, 4326)), \  
-                     \room_id \
-                     \from users where user_id = ?"  -- st_x is lng, st_y is lat
+    [r] <- quickQuery' dbh 
+                     ("select " ++ userFields ++ " from users where user_id = ?")  -- st_x is lng, st_y is lat
                      [toSql uid]
-    return $ User { userId = fromSql a
-                  , nickName = fromSql b
-                  , userCoordinates = (getCoordinates (fromSql lat) (fromSql lng))
-                  , userRoomId = fromSql c
-                  }
+    return $ convUserRow r
+            
+userFields = "user_id, name, \
+    \st_y(st_transform(coordinates, 4326)), \
+    \st_x(st_transform(coordinates, 4326)), room_id"
+         
+convUserRow :: [SqlValue] -> User
+convUserRow [a,b,lat,lng,c] = 
+    User { userId = fromSql a
+         , nickName = fromSql b
+         , userCoordinates = (getCoordinates (fromSql lat) (fromSql lng))
+         , userRoomId = fromSql c
+         }
   where getCoordinates :: Maybe Double -> Maybe Double -> Maybe Coordinates
         getCoordinates (Just lat') (Just lng') = Just (lat', lng') 
         getCoordinates _ _ = Nothing
+
+
